7a2fa3338a7c7e1567e6feb29b628157fe272d72
mins1031 <urisegea@naver.com>
2025-01-08 01:37:08 +0900

    첫 커밋

---
+++ src/main/java/com/example/productbatch/ProductbatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.productbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductbatchApplication.class, args);
+	}
+
+}
